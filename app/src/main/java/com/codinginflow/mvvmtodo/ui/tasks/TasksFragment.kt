3d8fa2146ce1b2816d3c83d83488bdc57100bc97
Pranav Patel <ppatel@dataminr.com>
2021-05-10 19:18:30 -0400

    Creating list adapter, fragment viewmodel bindings
    Creating a Tasks recycler view adapter that binds the task to the ItemTaskView
    and creates appropirate functions to handle changes and setup.  Updating
    TasksFragment to bind to its FragmentTask view and also binding the
    recyclerview to set current task adapter, layout manager and setHasFixedSize to
    handle orientation changes and setup TaskViewModel to observe the taskAdapter
    list. Fixes item task text view bug.

--- app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksFragment.kt
@@ -1,12 +1,35 @@
 package com.codinginflow.mvvmtodo.ui.tasks
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.codinginflow.mvvmtodo.R
+import com.codinginflow.mvvmtodo.databinding.FragmentTasksBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class TasksFragment : Fragment(R.layout.fragment_tasks) {
 
     private val viewModel: TasksViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentTasksBinding.bind(view)
+        val tasksAdapter = TasksAdapter()
+
+        binding.apply {
+            recyclerViewTasks.apply {
+                adapter = tasksAdapter
+                layoutManager = LinearLayoutManager(requireContext())
+                setHasFixedSize(true)
+            }
+        }
+
+        viewModel.tasks.observe(viewLifecycleOwner, Observer { tasksAdapter.submitList(it) })
+    }
+
 }
