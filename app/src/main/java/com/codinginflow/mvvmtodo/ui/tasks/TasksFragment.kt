b7a178414d782e1c22598c19984c00e9d093169d
Pranav Patel <ppatel@dataminr.com>
2021-05-11 18:44:00 -0400

    Creating nav bar menu with a functional search.
    Created menu xml file to hold UI menu options. Implementing task fragment menu
    option functions. Creating a SearchView inline extension function that
    implements setOnQueryTextListener functions. Updating TaskDao to use search
    query to retrieve tasks. Updating TasksViewModel to use flow and updates the
    tasks based on search query provided by task fragment.

--- app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksFragment.kt
@@ -1,13 +1,18 @@
 package com.codinginflow.mvvmtodo.ui.tasks
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.codinginflow.mvvmtodo.R
 import com.codinginflow.mvvmtodo.databinding.FragmentTasksBinding
+import com.codinginflow.mvvmtodo.util.onQueryTextChanged
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -30,6 +35,40 @@ class TasksFragment : Fragment(R.layout.fragment_tasks) {
         }
 
         viewModel.tasks.observe(viewLifecycleOwner, Observer { tasksAdapter.submitList(it) })
+
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_fragment_tasks, menu)
+
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.onQueryTextChanged {
+             // update search when text changes.
+            viewModel.searchQuery.value = it
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.action_search -> {
+
+                true
+            }
+            R.id.action_sort_by_name -> {
+                true
+            }
+            R.id.action_hide_completed_tasks -> {
+                item.isChecked = !item.isChecked
+                true
+            }
+            R.id.action_delete_all_completed_tasks -> {
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
 }
---
+++ app/src/main/java/com/codinginflow/mvvmtodo/util/ViewExt.kt
@@ -0,0 +1,14 @@
+package com.codinginflow.mvvmtodo.util
+
+import androidx.appcompat.widget.SearchView
+
+inline fun SearchView.onQueryTextChanged(crossinline listener: (String) -> Unit) {
+    this.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+        override fun onQueryTextSubmit(query: String?) = true
+
+        override fun onQueryTextChange(newText: String?): Boolean {
+            listener(newText.orEmpty())
+            return true
+        }
+    })
+}
